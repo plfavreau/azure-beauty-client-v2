4478e8f6be4c9ef4e7b9c04db4db8796f55ea220
ilyas <oulkaddailyas@gmail.com>
2024-06-30 15:19:29 +0200

    update endpoint to use local instance

--- src/app/services/azurePredictionService.ts
+++ src/app/services/azurePredictionService.ts
@@ -5,19 +5,17 @@ export async function makePrediction(
 ): Promise<PredictionResponse | undefined> {
   const base64Response = await fetch(image);
   const blob = await base64Response.blob();
+  const url = 'http://localhost:8080/image'
 
-  const myHeaders = new Headers();
-  myHeaders.append('Prediction-Key', 'fec306f015044fde8a6c87b9a5adbbc7');
 
   const requestOptions = {
     method: 'POST',
-    headers: myHeaders,
     body: blob,
   };
 
   try {
     const response = await fetch(
-      'https://projetazureprediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/eaec1732-b7f7-462b-baf8-46d9ba7aedc2/classify/iterations/Iteration2/image',
+      url,
       requestOptions,
     );
 
